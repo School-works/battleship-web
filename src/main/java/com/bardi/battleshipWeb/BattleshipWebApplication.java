1ecbe4fc5c95e274d7b46fa5311db70c735fa3b5
bad0dab <broadnicegamerr@gmail.com>
2025-05-05 06:12:48 +0200

    first commit

---
+++ src/main/java/com/bardi/battleshipWeb/BattleshipWebApplication.java
@@ -0,0 +1,13 @@
+package com.bardi.battleshipWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleshipWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleshipWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bardi/battleshipWeb/BattleshipWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bardi.battleshipWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
